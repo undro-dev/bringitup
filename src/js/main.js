e798a4753cbd39abe89f1a3eaf5e9f5b38c43d2c
Vitaly Undro <vitalyundro@gmail.com>
2022-01-09 19:10:26 +0300

    feat: add slider to the site

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import Slider from './modules/slider';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const slider = new Slider('.page');
+  const slider = new Slider('.page', '.next');
   slider.render();
 
 });
