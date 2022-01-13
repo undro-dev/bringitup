44e533fc4e06644f68dde941525568b0a5c291e0
Vitaly Undro <vitalyundro@gmail.com>
2022-01-13 22:33:12 +0300

    feat: add accordeon

--- src/js/modules/download.js
+++ src/js/modules/download.js
@@ -6,18 +6,13 @@ export default class Download {
 
   downloadItem(path) {
     const element = document.createElement('a');
-
     element.setAttribute('href', path);
     element.setAttribute('download', 'nice_picture');
-
     element.style.display = 'none';
     document.body.appendChild(element);
-
     element.click();
-
     document.body.removeChild(element);
   }
-
   init() {
     this.btns.forEach(item => {
       item.addEventListener('click', () => {
